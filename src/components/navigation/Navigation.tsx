87c137ff527e3cc05e04027c4f0e2961b633ff17
Julien Goldy <juliengoldy@Juliens-MacBook-Pro.local>
2024-02-18 03:39:49 +0100

    Adding logo

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 import { ThemeSwitch } from "./themeSwitch";
@@ -5,8 +6,15 @@ import { ThemeSwitch } from "./themeSwitch";
 function Navigation() {
   return (
     <nav className="flex justify-center w-full fixed top-0 py-3 border-b-2 backdrop-filter backdrop-blur-l">
-      <div className="flex justify-between max-w-6xl w-[90%]">
-        <p>NAV</p>
+      <div className="flex align-middle justify-between max-w-6xl w-[90%]">
+        <Image
+          className="relative dark:invert opacity-80 transition-all duration-200 ease-in-out hover:scale-110 hover:opacity-100"
+          src="/saki-logo.svg"
+          alt="Next.js Logo"
+          width={60}
+          height={10}
+          priority
+        />
         <ThemeSwitch />
       </div>
     </nav>
